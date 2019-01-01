bb25ab467caa40aa4db93441a03673d1eb5558e8
miladink <mi.aghajohari@gmail.com>
2019-01-01 14:35:11 +0330

    init

---
+++ Phase1_ML_Project/src/pre-process.py
@@ -0,0 +1,8 @@
+import nltk
+# nltk.download('punkt')
+nltk.download('wordnet')
+sentence = """Rasool hates machine learning and he doesn't have the 
+confidence needed to apply for it"""
+d = nltk.word_tokenize(sentence)
+print(d)
+string.punc
